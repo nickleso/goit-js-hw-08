d9ec508a61371c667f10e55740b02f120b5654d8
Nick Leso <nickleso.work@gmail.com>
2022-08-29 20:23:09 +0300

    Task 2
    - add video time saver

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+const VIDEOPLAYER_CURRENT_TIME = 'videoplayer-current-time';
+
+// video Title
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+// current time
+function currentTime({ seconds }) {
+  const timeToStart = Math.round(seconds);
+  localStorage.setItem(VIDEOPLAYER_CURRENT_TIME, timeToStart);
+}
+
+player.on('timeupdate', currentTime);
+
+const savedTime = localStorage.getItem(VIDEOPLAYER_CURRENT_TIME);
+console.log(`Pause on: ${savedTime} seconds`);
+
+player
+  .setCurrentTime(savedTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
